d7320fdce2d377c7f7adddfbdabff91c9337ca79
chloebyrnes <107075274+chloebyrnes@users.noreply.github.com>
2022-06-29 23:26:21 -0400

    added prompts for numbers letters symbols

--- assests/script.java
+++ assests/script.java
@@ -1,13 +1,23 @@
 // Assignment code here
 var generateBtn = document.querySelector("generate");
 
-//initial prompt
+//initial prompt to enter length, numbers,
 function generatePassword() {
   var userInput = window.prompt("How long would you like your password to be?")
   var passwordLength = parseInt(userInput)
   if (isNaN(passwordLength)) {
     window.alert("Please enter a number.")
-  }
+    return
+  } 
+
+  if (passwordLength < 8 || passwordLength > 128) {
+  window.alert("Password length must be between 8 and 128 characters")
+  return
+  } 
+  var userwantsNumbers = window.confirm("Would you like to include numbers in your password?")
+  var userwantsSymbols = window.confirm("Would you like to include symbols in your password?")
+  var userwantsLowercase = window.confirm("Would you like to include lowercase letters in your password?")
+  var userwantsUppercase = window.confirm("Would you like to include uppercase letters in your password?")
 }
 
 // Write password to the #password input
